fe5883a92389f1ea7c38334a2d763e0db54505fd
Jp <kalambayjp@gmail.com>
2021-11-24 01:18:15 +0000

    added array equality function

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const assertEqualArray = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  return arr1 = arr1.toString() === arr2.toString();
+};
+
