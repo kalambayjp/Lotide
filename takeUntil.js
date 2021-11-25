6d416481cd2465a3743aa5619f7c12991101985d
Jp <kalambayjp@gmail.com>
2021-11-25 23:33:55 +0000

    implement takeUntil challenge

---
+++ takeUntil.js
@@ -0,0 +1,23 @@
+const assertEqualArray = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  return arr1 = arr1.toString() === arr2.toString();
+};
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (const item of array) {
+    if (callback(item)) {
+      break;
+    } else {
+      result.push(item);
+    }
+  }
+  return result;
+};
