e90dd4cfa89cd5a893ee104fb1b3dbb5f6d117ed
Jp <kalambayjp@gmail.com>
2021-11-25 22:46:30 +0000

    our own version of map

---
+++ map.js
@@ -0,0 +1,28 @@
+const assertEqualArray = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  return arr1 = arr1.toString() === arr2.toString();
+};
+
+const map = function(array, callback) {
+  const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+    results.push(callback(item));
+  }
+  return results;
+};
+
+const words = [1,2,3,];
+const results1 = map(words, word => word + '!');
+
+console.log(assertEqualArray(results1, ['1!', '2!', '3!']));
+
