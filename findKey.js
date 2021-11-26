8cc63c50eebfb239baa36f329dd916708e45276b
Jp <kalambayjp@gmail.com>
2021-11-26 00:07:32 +0000

    d4 implement findKey challenge

---
+++ findKey.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(object, callback) {
+  for (key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+};
