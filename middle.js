c709394c54adf09733fa38f44b5495fcd892668d
JP <kalambayjp@gmail.com>
2021-11-29 13:50:06 -0800

    refactored and tested middle function

--- middle.js
+++ middle.js
@@ -1,15 +1,3 @@
-const assertEqualArray = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  return arr1 = arr1.toString() === arr2.toString();
-};
-
 const middle = function(arr) {
   let middle = [];
   if (arr.length > 2) {
@@ -22,3 +10,5 @@ const middle = function(arr) {
   }
   return middle;
 };
+
+module.exports = middle;
