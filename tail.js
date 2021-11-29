234f7d89d8f2dc44722e7f9a5d364801ea6c9133
JP <kalambayjp@gmail.com>
2021-11-29 13:40:47 -0800

    refactored and tested tail function

--- tail.js
+++ tail.js
@@ -1,22 +1,11 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(input) {
-  const tail = input.slice(1);
-  return tail;
+  return input.slice(1);
 };
 
-const words = ['hello', 'there', 'friend'];
-const result = tail(words);
 
-assertEqual(result.length, 2);
+module.exports = tail;
 
-console.log(words);
+
 
 
 
